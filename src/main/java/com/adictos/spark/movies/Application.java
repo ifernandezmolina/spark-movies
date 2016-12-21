c0fe25a1136aac45db76d53e6371881d59d3dd8b
ifernandezmolina <ifernandez@macbook-ifernandez.local>
2016-12-21 10:39:56 +0100

    no message

--- src/main/java/com/adictos/spark/movies/Application.java
+++ src/main/java/com/adictos/spark/movies/Application.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.adictos.spark.movies.config.DatabaseConfig;
 import com.adictos.spark.movies.service.MoviesService;
 
 @Configuration
@@ -11,7 +12,7 @@ import com.adictos.spark.movies.service.MoviesService;
 public class Application {
 
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Application.class);
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Application.class, DatabaseConfig.class);
     	new MoviesApplication(ctx.getBean(MoviesService.class));
         ctx.registerShutdownHook();
 	}
